82a47017f15570bcbaf8618189880426d70054d4
Thilina Ruwan <106557565+thilinaruwan112@users.noreply.github.com>
2025-02-21 16:39:26 +0530

    Update config.js

--- client/config.js
+++ client/config.js
@@ -1,9 +1,11 @@
 // config.js
 const config = {
-  API_BASE_URL: "http://localhost/pharma-college-project/server",
+  API_BASE_URL: "https://qa-api.pharmacollege.lk",
   ADMIN_BASE_URL: "https://kdu-admin.payshia.com",
-  CERTIFICATE_URL: "http://localhost/pharma-college-project/certificate-generator",
-  CERTIFICATE_IMG_URL: "https://content-provider.pharmacollege.lk/content-provider/certificates",
+  CERTIFICATE_URL:
+    "http://localhost/pharma-college-project/certificate-generator",
+  CERTIFICATE_IMG_URL:
+    "https://content-provider.pharmacollege.lk/content-provider/certificates",
   // API_BASE_URL: "http://localhost/TeaJarWebsite/server",
   // ADMIN_BASE_URL: "http://localhost/TeaJarWebsite/admin",
 };
