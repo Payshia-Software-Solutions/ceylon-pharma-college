73d4e41664d7507a5a4bc969fa2a7c57524e3b96
thilinaruwan11 <ict20838@fot.sjp.ac.lk>
2025-03-23 12:03:11 +0530

    update video

--- client/app/course/page.jsx
+++ client/app/course/page.jsx
@@ -4,11 +4,7 @@ import Course from  "@/components/Course/Course";
 function page() {
   return (
     <div className='mt-28'>
-
       <div className='mt-14'>  <Course/></div>
-
-      
-      
     </div>
   )
 }
