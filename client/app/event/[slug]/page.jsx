2a736c98b991e33e3a16dd3e088bbf9bc953697d
thilinaruwan11 <ict20838@fot.sjp.ac.lk>
2025-04-10 20:15:06 +0530

    update events

--- client/app/event/[slug]/page.jsx
+++ client/app/event/[slug]/page.jsx
@@ -4,10 +4,8 @@ import config from "@/config";
 
 export async function generateMetadata({ params }) {
   const { slug } = params;
-  
   try {
     const eventData = await getEventBySlug(slug);
-    
     return {
       title: eventData ? `${eventData.title} | Events` : 'Event Details',
       description: eventData ? eventData.description.substring(0, 160) : 'View event details.',
@@ -21,31 +19,23 @@ export async function generateMetadata({ params }) {
   }
 }
 
-
 async function getEventBySlug(slug) {
   const apiUrl = `${config.API_BASE_URL}/events-page/${slug}`;
   const response = await fetch(apiUrl, { next: { revalidate: 3600 } }); 
-  
   if (!response.ok) {
     throw new Error(`Event not found. Status: ${response.status}`);
-  }
-  
+  }  
   return response.json();
 }
 
 
 export async function generateStaticParams() {
   try {
-
     const res = await fetch(`${config.API_BASE_URL}/events-page`, { next: { revalidate: 3600 } });
-    
     if (!res.ok) {
       throw new Error("Failed to fetch events");
     }
-    
     const events = await res.json();
-    
-
     return events.map((event) => ({
       slug: event.slug,
     }));
@@ -57,7 +47,5 @@ export async function generateStaticParams() {
 
 export default async function EventPage({ params }) {
   const { slug } = params;
-  
-
   return <SingleEvent slug={slug} />;
 }
